8c7cb7e70ae5c60a74aea01a18ad915225723140
Semere Habtu <se.semere.habtu@gmail.com>
2021-05-26 11:28:15 +0300

    Base repository

---
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -0,0 +1,17 @@
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Models
+{
+
+    [CourseTitleMustBeDifferentFromDescriptionAttribute(
+        ErrorMessage = "Title must be different from description")]
+    public class CourseForCreationDto // : IValidatableObject
+    {
+
+    }
+}
---
+++ CourseLibrary.API/Models/CourseForManipulationDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Models
+{
+    public abstract class CourseForManipulationDto
+    {
+
+        [Required(ErrorMessage = "You should fill out a title.")]
+
+        [MaxLength(100, ErrorMessage = "The title shouldn't be more than 100 characters.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "You should fill out a title.")]
+
+        [MaxLength(1500, ErrorMessage = "The description shouldn't be more than 1500 characters.")]
+        public virtual string Description { get; set; }
+
+    }
+}
