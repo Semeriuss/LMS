c0295ead92a5ccdf994c58f1d77e529d16d503d4
Semere Habtu <se.semere.habtu@gmail.com>
2021-06-02 13:11:14 +0300

    Media Controller

--- CourseLibrary.API/Controllers/MediaController.cs
+++ CourseLibrary.API/Controllers/MediaController.cs
@@ -10,33 +10,25 @@ using System.Threading.Tasks;
 public class VideosController : ControllerBase
 {
     [HttpGet]
-    public HttpResponseMessage Get(string filename, string ext)
+    public IActionResult Get(string filename, string ext)
     {
-        var video = new VideoStream(filename, ext);
-
-        var result = new HttpResponseMessage(HttpStatusCode.OK)
-        {
-            Content = new PushStreamContent(video.WriteToStream, new MediaTypeHeaderValue("application/octet-stream" + ext)),
-        };
-
-        result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-        return result;
-
+        string path = @"C:\Users\semer\Downloads\Video\1.mp4";
+        return PhysicalFile(path, "application/octet-stream", enableRangeProcessing: true);
     }
 
     [HttpGet]
     [Route("Download")]
-    public IActionResult Download()
+    public async Task<IActionResult> Download()
     {
         string path = @"C:\Users\semer\Downloads\Video\1.mp4";
-        //var memory = new MemoryStream();
-        //using (FileStream stream = new FileStream(@"C:\Users\semer\Downloads\Video\1.mp4", FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 65536, FileOptions.Asynchronous | FileOptions.SequentialScan))
-        //{
-        //    await stream.CopyToAsync(memory);
-        //}
-        //memory.Position = 0;
-        //return File(memory, "application/octet-stream", Path.GetFileName(path), true); //enableRangeProcessing = true
-        return PhysicalFile(path, "application/octet-stream", enableRangeProcessing: true);
+        var memory = new MemoryStream();
+        using (FileStream stream = new FileStream(@"C:\Users\semer\Downloads\Video\1.mp4", FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 65536, FileOptions.Asynchronous | FileOptions.SequentialScan))
+        {
+            await stream.CopyToAsync(memory);
+        }
+        memory.Position = 0;
+        return File(memory, "application/octet-stream", Path.GetFileName(path), true); //enableRangeProcessing = true
+
     }
 
 
